a9245f217154b7287405b18db2d7c48f1e7bb0d8
BryceTuppurainen <bryce.tuppurainen.au@gmail.com>
2022-08-03 09:04:23 +1000

    docs: Added developer boilerplate

---
+++ dev/Boilerplate.jsx
@@ -0,0 +1,18 @@
+// * DELETE UNUSED CONTENT AS YOU GO
+
+// External Imports (Not from files that belong to this project)
+import { React, useState } from "react";
+
+// Internal Imports
+import { getBoilerplate } from "./GetBoilerplate";
+
+// Use const functions rather than classes
+const Boilerplate = () => {
+	// Use useState to create stateful variables
+	const [boilerplate, setBoilerplate] = useState([]);
+
+	// Return JSX
+	return <></>;
+};
+
+export default Boilerplate;

30c2e4bf64ca24e64c1a598381b77a42d734350f
BryceTuppurainen <bryce.tuppurainen.au@gmail.com>
2022-08-03 09:05:05 +1000

    feat: Updated index to use Routes

--- flightpub/src/index.js
+++ flightpub/src/index.js
@@ -1,9 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-// TODO(BryceTuppurainen): Link modules
+import "./index.css";
+import Flights from "./Flights/Flights";
+
+// TODO(BryceTuppurainen): Implement all of the original pages
+// import Flight from "./Flight";
+// import Destinations from "./Destinations";
+// import Destination from "./Destination";
+// import Help from "./Help";
+// import SignIn from "./SignIn";
+// import SignUp from "./SignUp";
+// import Account from "./Account";
+// import Booking from "./Booking";
+// import Checkout from "./Checkout";
+// import Group from "./Group";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<React.StrictMode></React.StrictMode>);
+
+root.render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Flights />} />
+			</Routes>
+		</BrowserRouter>
+	</React.StrictMode>
+);

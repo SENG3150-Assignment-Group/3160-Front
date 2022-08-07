fb37d137059a269d096dd3bf372d2cb5b3c01d17
BryceTuppurainen <bryce.tuppurainen.au@gmail.com>
2022-08-08 09:11:07 +1000

    feat: Implemented the Common/Header

---
+++ flightpub/src/Common/Header.jsx
@@ -0,0 +1,75 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import "./HeaderStyles.css";
+
+// TODO(BryceTuppurainen): Implement authentication
+
+const isAuthenticated = true;
+
+const Header = ({ page }) => {
+	let navigate = useNavigate();
+
+	return (
+		<header>
+			<div className="Header">
+				<div className="Logo">
+					<div>
+						<img alt="FlightPub Logo" src="./favicon.ico" />
+					</div>
+
+					<div>
+						<h1>Flight Pub</h1>
+					</div>
+				</div>
+
+				<nav>
+					<div>
+						<a href="/flights">Flights</a>
+					</div>
+
+					<div>
+						<a href="/destinations">Explore</a>
+					</div>
+
+					<div>
+						<a href="/group">Travel Groups</a>
+					</div>
+				</nav>
+
+				{isAuthenticated ? (
+					<div className="AuthenticationOptions">
+						<div></div>
+						<div>
+							<a href="/account">My Account</a>
+						</div>
+						<div>
+							<a
+								onClick={() => {
+									localStorage.setItem("email", "");
+									navigate("/");
+								}}
+							>
+								Sign Out
+							</a>
+						</div>
+					</div>
+				) : (
+					<div className="AuthenticationOptions">
+						<div></div>
+						<div>
+							<a href="/authentication?q=sign-in">Sign In</a>
+						</div>
+
+						<div>
+							<a href="/authentication?q=sign-up">Sign Up</a>
+						</div>
+					</div>
+				)}
+			</div>
+			<hr />
+		</header>
+	);
+};
+
+export default Header;
--- flightpub/src/Flights/Flights.jsx
+++ flightpub/src/Flights/Flights.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
+import Header from "../Common/Header";
+
 const Flights = () => {
-	const [flights, setFlights] = useState({ flights: [] });
+	const [flights, setFlights] = useState({
+		flights: []
+	});
 
 	useEffect(() => {
 		// TODO(BryceTuppurainen): Write a fetch in order to retrieve the flights from the database on startup
@@ -10,27 +14,11 @@ const Flights = () => {
 
 	// TODO(): Add any relevant functions in here for the management of the flight search page (authentication, suggested flights, etc.)
 
+	// TODO(BryceTuppurainen): Implement the flight search criterion and display window
+
 	return (
 		<>
-			<h1>Flights Search Placeholder</h1>
-			<p>
-				Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsum
-				inventore aperiam reprehenderit. Esse dicta quo aperiam officiis
-				cupiditate perferendis vitae dolor eligendi deserunt fugit, odit
-				eveniet, vero omnis? Cupiditate, veniam. Lorem ipsum dolor sit
-				amet consectetur, adipisicing elit. Ipsum inventore aperiam
-				reprehenderit. Esse dicta quo aperiam officiis cupiditate
-				perferendis vitae dolor eligendi deserunt fugit, odit eveniet,
-				vero omnis? Cupiditate, veniam. Lorem ipsum dolor sit amet
-				consectetur, adipisicing elit. Ipsum inventore aperiam
-				reprehenderit. Esse dicta quo aperiam officiis cupiditate
-				perferendis vitae dolor eligendi deserunt fugit, odit eveniet,
-				vero omnis? Cupiditate, veniam. Lorem ipsum dolor sit amet
-				consectetur, adipisicing elit. Ipsum inventore aperiam
-				reprehenderit. Esse dicta quo aperiam officiis cupiditate
-				perferendis vitae dolor eligendi deserunt fugit, odit eveniet,
-				vero omnis? Cupiditate, veniam.
-			</p>
+			<Header />
 		</>
 	);
 };

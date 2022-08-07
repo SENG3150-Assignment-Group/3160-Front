9917113fa4de56823b22a944eb341a150c64c992
BryceTuppurainen <bryce.tuppurainen.au@gmail.com>
2022-08-07 12:21:39 +1000

    feat: Add account

--- flightpub/src/index.js
+++ flightpub/src/index.js
@@ -12,7 +12,7 @@ import Flights from "./Flights/Flights";
 // import Help from "./Help";
 // import SignIn from "./SignIn";
 // import SignUp from "./SignUp";
-// import Account from "./Account";
+import Account from "./Account/Account";
 // import Booking from "./Booking";
 // import Checkout from "./Checkout";
 // import Group from "./Group";
@@ -24,6 +24,17 @@ root.render(
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<Flights />} />
+				<Route path="/flights" element={<Flights />} />
+				<Route path="/account" element={<Account />} />
+				{
+					// TODO(BryceTuppurainen): Implement all of the original pages and link here
+					/* <Route path="/" element={<Flights />} />
+				<Route path="/" element={<Flights />} />
+				<Route path="/" element={<Flights />} />
+				<Route path="/" element={<Flights />} />
+				<Route path="/" element={<Flights />} />
+				<Route path="/" element={<Flights />} /> */
+				}
 			</Routes>
 		</BrowserRouter>
 	</React.StrictMode>

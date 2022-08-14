add1e1b0315fafda67055161077163a46ba73087
BryceTuppurainend <bryce.tuppurainen.au@gmail.com>
2022-08-14 17:07:57 +1000

    feat: Implement three new blank pages

---
+++ flightpub/src/Agent/Agent.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Header from "../Common/Header/Header";
+
+const Agent = () => {
+	return (
+		<>
+			<Header />
+		</>
+	);
+};
+
+export default Agent;
---
+++ flightpub/src/Staff/Staff.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Header from "../Common/Header/Header";
+
+const Staff = () => {
+	return (
+		<>
+			<Header />
+		</>
+	);
+};
+
+export default Staff;

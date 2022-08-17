3c6d4c97054f5229748c8e39c0405927f7fe2fcd
BryceTuppurainen <bryce.tuppurainen.au@gmail.com>
2022-08-17 12:50:45 +1000

    hack: Adds some automagic! :sparkle

--- flightpub/src/Authentication/Authentication.jsx
+++ flightpub/src/Authentication/Authentication.jsx
@@ -1,11 +1,26 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 import Header from "../Common/Header/Header";
 
 const Authentication = () => {
+	let navigate = useNavigate();
+
 	return (
 		<>
 			<Header />
+			<p
+				onClick={() => {
+					localStorage.setItem("fullname", "FlightPub User");
+					localStorage.setItem("email", "user.name@email.com");
+					localStorage.setItem("password", "SuperStrongPassword1!");
+					localStorage.setItem("permission", "1"); // i.e. a user
+					navigate("/account");
+				}}
+			>
+				!!! Click this magic link text to automagically log in (yes this
+				is going to be fixed in future) !!!
+			</p>
 		</>
 	);
 };
